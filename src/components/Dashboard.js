63b320a755fee32ea7a85959cdbf04ab5c392fe3
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-09-25 14:34:19 -0700

    feat: add panel component with mock data

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,6 +2,30 @@ import React, { Component } from "react";
 
 import classnames from "classnames";
 import Loading from "./Loading";
+import Panel from "./Panel";
+
+const data = [
+  {
+    id: 1,
+    label: "Total Photos",
+    value: 10
+  },
+  {
+    id: 2,
+    label: "Total Topics",
+    value: 4
+  },
+  {
+    id: 3,
+    label: "User with the most uploads",
+    value: "Allison Saeng"
+  },
+  {
+    id: 4,
+    label: "User with the least uploads",
+    value: "Lukas Souza"
+  }
+];
 
 class Dashboard extends Component {
   state = {
@@ -10,12 +34,19 @@ class Dashboard extends Component {
 
   render() {
     const dashboardClasses = classnames("dashboard");
+    const panels = data.map(panel => {
+      return <Panel key={panel.id} id={panel.id} label={panel.label} value={panel.value} />;
+    });
 
     if (this.state.loading) {
       return <Loading />;
     }
 
-    return <main className={dashboardClasses} />;
+    return (
+      <main className={dashboardClasses}>
+        {panels}
+      </main>
+    );
   }
 }
 
