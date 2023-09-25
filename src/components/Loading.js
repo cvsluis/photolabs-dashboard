bdae551ca7ccef8e9a2585ee367854bd3864e2c6
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-09-25 14:26:19 -0700

    feat: add loading component

---
+++ src/components/Loading.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import classnames from "classnames";
+
+class Loading extends Component {
+  render() {
+    return <section className="loading">Loading</section>;
+  }
+}
+
+export default Loading;
